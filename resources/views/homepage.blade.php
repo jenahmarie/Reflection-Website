a9fab6b439b63e914af114b10fb227fe2031263b
jenahmarie <jenahmarie28@gmail.com>
2025-01-14 12:57:09 +0800

    change title

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -2,7 +2,7 @@
 <head>
   <meta charset="utf-8"/>
   <meta content="width=device-width, initial-scale=1.0" name="viewport"/>
-  <title>Frontend Developer Portfolio</title>
+  <title>HOMEPAGE</title>
   <script src="https://cdn.tailwindcss.com"></script>
   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet"/>
   <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet"/>
